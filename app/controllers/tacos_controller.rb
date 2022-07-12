8889b8a207a80b2d3d784e4a5e5f0ec0d0d4698f
James <jimibue@gmail.com>
2022-07-12 10:46:40 -0600

    cloudinary

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,2 +1,34 @@
 class TacosController < ApplicationController
+
+    def new_user
+        binding.pry
+        # getting file from client
+        file = params[:file]
+        name = params[:name]
+        begin
+            # save imagine to cloudinary
+            cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true, resource_type: :auto)
+            # succesfull saved to cloudinary add to db
+            user = User.new(user_params)
+            user.image_url = cloud_image[":secure_url"]
+            if(user.save)
+                # we success create user to our db
+                render json: user
+            else
+                # we unsuccessfully create user to our db
+                render json: {errors: user.errors.full_messages}, status: 422
+            end
+        
+        rescue => e
+            # if this is unsucessfully saved to cloudinary than we will come here
+            # and e will give us an idea about why it didn't
+            render json: {errors: e}, status: 422
+        end
+    end
+
+    private
+
+    def user_params
+       params.require(:user).permit(:name)
+    end
 end
